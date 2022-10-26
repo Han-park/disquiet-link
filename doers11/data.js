4af95eb0625b92e775477b6af13f3e09ebac0fd6
Han Park <hanpark@Hani-Book-Pro-13.local>
2022-10-27 07:25:40 +0900

    doers11

---
+++ doers11/data.js
@@ -0,0 +1,98 @@
+export default [
+    {
+      "no": 1,
+      "profileImg": "images/profile/f7073fb0eb5b8d991820c2b827e0840d30ae712866f711c16f936c61ea4588ec",
+      "name": "Hyunsol Park",
+      "where": "CEO @Disquiet",
+      "description": "IT 프로덕트 메이커들을 위한 소셜 네트워킹 플랫폼 '디스콰이엇'",
+      "profileLink": "https://disquiet.io/@hpark0011"
+    },
+    // {
+    //   "no": 2,
+    //   "profileImg": "",
+    //   "name": "김정민",
+    //   "where": "CEO @LRHR",
+    //   "description": "전국 명품 수선 장인들을 한 곳에 모은 명품 수선 플랫폼 '패피스'",
+    //   "profileLink": ""
+    // },
+    {
+      "no": 3,
+      "profileImg": "images/profile/ee9a795cf2797d1356f8778a609d90a43f0c97b098892f24eb5a07d7de50acdf",
+      "name": "Shawny Kwon",
+      "where": "CEO @Munice Inc.",
+      "description": "고효율 수면을 제공하는 뇌과학 기반 수면 서비스 '미라클나잇'",
+      "profileLink": "https://disquiet.io/@shawnice"
+    },
+    {
+      "no": 4,
+      "profileImg": "images/profile/cc4dfc318e30f1e0ab89b9455a03372548e1b6444876777e88d118287155204f",
+      "name": "선민승",
+      "where": "CEO @Splab Inc",
+      "description": "나와 우리 팀을 위한 비즈니스 미팅 관리 솔루션 '스플랩'",
+      "profileLink": "https://disquiet.io/@splab_ceo"
+    },
+    {
+      "no": 5,
+      "profileImg": "images/profile/0edfa2d01406a70050bbf70eb11db79aff61cbf846b3e06f689090f5406ac632",
+      "name": "Woncheol Jung",
+      "where": "CEO @Camelotech",
+      "description": "단 3분만에 한약을 달이는 스마트 탕제 시스템 '카멜로테크'",
+      "profileLink": "https://disquiet.io/@ralph"
+    },
+    {
+      "no": 6,
+      "profileImg": "images/profile/871ea0c75dbfde74912423ce42e09f4cc31fbab461e3385ecd715fb68ca25408",
+      "name": "이정열",
+      "where": "CEO @나비효과",
+      "description": "유튜브 영상 속 아이템 서치 & 쇼핑 서비스 '봄봄'",
+      "profileLink": "https://disquiet.io/@lee_passionate_"
+    },
+    {
+      "no": 7,
+      "profileImg": "images/profile/04bec58690e7d60e49cad91bacdd207c83286a96e69f0128739cdef1088d542f",
+      "name": "김정은",
+      "where": "CEO @알밤",
+      "description": "AR게임 형태의 디지털 치료 메타버스 플랫폼 '잼재미 AR'",
+      "profileLink": "https://disquiet.io/@queenjek"
+    },
+    {
+      "no": 8,
+      "profileImg": "images/profile/ec6cd61418dccd959705288771e59def2ada8b8faaac627a7bf45028848eb620",
+      "name": "권기현",
+      "where": "CEO @EcoTect",
+      "description": "저렴한 친환경 원료를 사용한 친환경 부표 'EcoFoam'",
+      "profileLink": "https://disquiet.io/@ecotect_gwon"
+    },
+    {
+      "no": 9,
+      "profileImg": "images/profile/7b6edd8e65cec7fac3d21f1f6d47e15c3af927e5a08b317b5a7d78a0459c4f01",
+      "name": "이민영",
+      "where": "CEO @와이낫에듀",
+      "description": "숏폼 문제 풀이 콘텐츠 앱 '알모'",
+      "profileLink": "https://disquiet.io/@flals9711"
+    },
+    {
+      "no": 10,
+      "profileImg": "images/profile/f0b5014b79e1afa666687abb61b34bb02ea453fed72264632888a51c78080fe0",
+      "name": "Hyeonji Hwang",
+      "where": "CEO @Sonus",
+      "description": "대화형 AI 기반 마음관리 솔루션",
+      "profileLink": "https://disquiet.io/@local"
+    },
+    {
+      "no": 11,
+      "profileImg": "images/profile/c9e4e1b82076768765167a48aba7ff2c1b424834d171a50099058b1a50d9c907",
+      "name": "김영호",
+      "where": "CEO @티엠알파운더스",
+      "description": "개인 자영업자들을 위한 창업 플랫폼 '내일의창업'",
+      "profileLink": "https://disquiet.io/@tmr-danny"
+    },
+    {
+      "no": 12,
+      "profileImg": "images/profile/1153778a01449add67d174ff564b07e99466489da5822fd21b12f7dfb749bd8b",
+      "name": "이경민",
+      "where": "CEO @PLUME.D",
+      "description": "AI 모션 시퀀스 기반 3D 아바타 렌더링 솔루션 'AvaTok'",
+      "profileLink": "https://disquiet.io/@logankm0307"
+    }
+  ]
